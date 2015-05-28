328a2e1f78ab0e07e0a55644c1e1dc9f4746101c
monika0603 <monika.pu@gmail.com>
2015-05-28 11:54:33 -0400

    Produces correction factors w.r.t. centralities

--- CorrectionFactor/CorrectionFactorsMacro.C
+++ CorrectionFactor/CorrectionFactorsMacro.C
@@ -1,31 +1,49 @@
 #include "CorrectionFactors_MinBias_PbPb.C"
+#include <TH1.h>
 
 void CorrectionFactorsMacro()
 {
-    
     CorrectionFactors_MinBias_PbPb *cf = new CorrectionFactors_MinBias_PbPb();
     
-    TCanvas *c1 = cf->Canvas();
     TFile *fRead = new TFile("TriHadronFirstAttempt_FullStat_Centralities.root");
-    TH1F *hdNdEta = Default(fRead);
-    hdNdEta->Draw();
     
-    TH1F *hUnSymmetrized = (TH1F *)hdNdEta->Clone();
-    TH1F *hSymmetrized = Symmetrize(hUnSymmetrized);
-    hSymmetrized->SetLineColor(kRed);
-    hSymmetrized->Draw("same");
+    const int nEtaBins = 51;
+    TH1F *hdNdEta[8];
+    const int centBin = 8;
     
-    TH1F *hdNdEta_0 = odNdEta(fRead);
-    hdNdEta_0->SetLineColor(kGreen+4);
-    hdNdEta_0->Draw("same");
+    for(int iCent_=0; iCent_<centBin; iCent_++)
+    {
+        hdNdEta[iCent_] = new TH1F(Form("hdNdEta_cent%d",iCent_), "hdNdEta", nEtaBins, -3.0, 3.0);
+    }
     
-    TCanvas *c2 = cf->Canvas1();
-    TH2F *hFactors = AlldNdEta(fRead, hSymmetrized);
-    hFactors->Draw("zcol");
+    for(int iCent_=0; iCent_<centBin; iCent_++)
+    {
+        hdNdEta[iCent_] = Default(fRead);
+    }
     
-    TFile *fNew = new TFile("CorrectionFactors_PbPb.root", "RECREATE");
-    hFactors->Write();
-    fNew->Close();
+    TH1F *hUnSymmetrized[8];
+    TH1F *hSymmetrized[8];
+    for(int iCent_=0; iCent_<centBin; iCent_++)
+    {
+        hSymmetrized[iCent_] = Symmetrize(hdNdEta[iCent_]);
+    }
+    
+    TH2F *hFactors[8];
+    
+    const double vZMin = -15.0;
+    const double vZMax = 15.0;
+    const int nVz = 15;
+    for(int iCent_=0; iCent_<centBin; iCent_++)
+    {
+        hFactors[iCent_] = new TH2F(Form("hFactors_cent%d",iCent_), "Efficiency; vZ vs eta", nEtaBins, -3.0, 3.0, nVz, vZMin, vZMax );
+    }
+    
+    
+    for(int iCent_=0; iCent_<centBin; iCent_++)
+    {
+        AlldNdEta(fRead, hSymmetrized[iCent_]);
+    }
+    
     
 }
 
