85e7ce832c0b90ca81a84d7ff9617eb4873e20d6
monika0603 <monika.pu@gmail.com>
2015-05-28 11:55:00 -0400

    Produces correction factors w.r.t. centralities

--- CorrectionFactor/CorrectionFactors_MinBias_PbPb.C
+++ CorrectionFactor/CorrectionFactors_MinBias_PbPb.C
@@ -17,10 +17,7 @@ class CorrectionFactors_MinBias_PbPb
     TH1F *Default(TFile *fRead);
     TH1F *odNdEta(TFile *fRead);
     TH1F *Symmetrize(TH1F *h);
-    TH2F *AlldNdEta(TFile *fRead, TH1F *hSymmetrized);
-    void WindowDressing();
-    TCanvas *Canvas();
-    TCanvas *Canvas1();
+    void AlldNdEta(TFile *fRead, TH1F *hSymmetrized);
     virtual ~CorrectionFactors_MinBias_PbPb();
     
   private:
@@ -30,6 +27,7 @@ class CorrectionFactors_MinBias_PbPb
 CorrectionFactors_MinBias_PbPb::CorrectionFactors_MinBias_PbPb( std::string fileName )
 {
     fRead = new TFile(fileName.c_str());
+    if(fRead) cout<<"Successfully opned the input file."<<endl;
 }
 
 CorrectionFactors_MinBias_PbPb::CorrectionFactors_MinBias_PbPb()
@@ -49,35 +47,38 @@ TH1F *Default(TFile *fRead)
 {
     TH1::SetDefaultSumw2();
     
+    TH1F *hdNdEta_default[8];
+    TH1F *hEvents[8];
     const float binWidth = 0.06;
+    char *cent0 = "0_10"; char *cent1 = "10_20"; char *cent2 = "20_30"; char *cent3 = "30_40";
+    char *cent4 = "40_50"; char *cent5 = "50_60"; char *cent6 = "60_70"; char *cent7 = "70_80";
     
-    TString _baseHistoName = "TriHadronAnalysisMultMinBias/hdNdEta_VzBin_7";
-    TH1F *hdNdEta_default  = (TH1F*)fRead->Get(_baseHistoName);
+    char *array[8];
     
-    TString _baseEventHisto = "TriHadronAnalysisMultMinBias/nEventsVzBin_7";
-    TH1F *hEvents = (TH1F *)fRead->Get(_baseEventHisto);
-    double nEvents_ = hEvents->GetEntries();
+    array[0] = cent0; array[1] = cent1; array[2] = cent2; array[3] = cent3; array[4] = cent4;
+    array[5] = cent5; array[6] = cent6; array[7] = cent7;
     
-    hdNdEta_default->Scale(1.0/nEvents_/binWidth);
+    double nEvents_[8]={-99,-99,-99,-99,-99,-99,-99,-99};
+    const int centBin = 8;
     
-    return hdNdEta_default;
-    
-}
+    for(int iCent_=0; iCent_<centBin; iCent_++)
+    {
+        char _baseHistoName[400];
+        sprintf(_baseHistoName,"TriHadronAnalysisMult%d/hdNdEta_VzBin_7",array[iCent_]);
+        cout<<_baseHistoName<<endl;
 
-TH1F *odNdEta(TFile *fRead)
-{
-    TH1::SetDefaultSumw2();
+        hdNdEta_default[iCent_]  = (TH1F*)fRead->Get(_baseHistoName);
     
-    const float binWidth = 0.06;
+        char _baseEventHisto[400];
+        sprintf(_baseEventHisto,"TriHadronAnalysisMult%d/hdNdEta_VzBin_7",array[iCent_]);
+        cout<<_baseEventHisto<<endl;
+
+        hEvents[iCent_] = (TH1F *)fRead->Get(_baseEventHisto);
+        nEvents_[iCent_] = hEvents[iCent_]->GetEntries();
     
-    TString _baseHistoName = "TriHadronAnalysisMultMinBias/hdNdEta_VzBin_0";
-    TH1F *hdNdEta_0  = (TH1F*)fRead->Get(_baseHistoName);
-    
-    TString _baseEventHisto = "TriHadronAnalysisMultMinBias/nEventsVzBin_0";
-    TH1F *hEvents = (TH1F *)fRead->Get(_baseEventHisto);
-    double nEvents_ = hEvents->GetEntries();
-    
-    return hdNdEta_0;
+        hdNdEta_default[iCent_]->Scale(1.0/nEvents_[iCent_]/binWidth);
+        return hdNdEta_default[iCent_];
+    }
     
 }
 
@@ -95,7 +96,7 @@ TH1F *Symmetrize(TH1F *h)
     return htemp;
 }
 
-TH2F *AlldNdEta(TFile *fRead, TH1F *hSymmetrized)
+void AlldNdEta(TFile *fRead, TH1F *hSymmetrized)
 {
     const float binWidth = 0.06;
     const int nBins = 14;
@@ -103,127 +104,72 @@ TH2F *AlldNdEta(TFile *fRead, TH1F *hSymmetrized)
     const double vZMin = -15.0;
     const double vZMax = 15.0;
     const int nVz = 15;
-    double ratio = 999;
+    double ratio[8] = {99,99,99,99,99,99,99,99};
+    const int centBin = 8;
     
-    TH2F *hVzEtaEff = new TH2F("hVzEtaEff_cent","hVzEtaEff;eta;vz", nEtaBins, -3.0, 3.0, nVz, vZMin, vZMax);
-    TH1F *hdNdEtaVzBin[15];
+    char *cent0 = "0_10"; char *cent1 = "10_20"; char *cent2 = "20_30"; char *cent3 = "30_40";
+    char *cent4 = "40_50"; char *cent5 = "50_60"; char *cent6 = "60_70"; char *cent7 = "70_80";
     
-    for(int iBin_=0; iBin_<=nBins; iBin_++)
+    char *array[8];
+    
+    array[0] = cent0; array[1] = cent1; array[2] = cent2; array[3] = cent3; array[4] = cent4;
+    array[5] = cent5; array[6] = cent6; array[7] = cent7;
+    
+    TH2F *hVzEtaEff[8];
+    TH1F *hdNdEtaVzBin[8][15];
+    TH1F * hEventsVzBin[8];
+    double nEvents[8];
+    double numerator[8];
+    double denominator[8];
+    
+    for(int iCent_=0; iCent_<centBin; iCent_++)
     {
-        for(int iEta_=0; iEta_<=nEtaBins; iEta_++)
+        hVzEtaEff[iCent_] = new TH2F(Form("hVzEtaEff_cent%d",iCent_), "Efficiency; vZ vs eta", nEtaBins, -3.0, 3.0, nVz, vZMin, vZMax);
+        
+        for(int iBin_=0; iBin_<=nBins; iBin_++)
         {
-            hVzEtaEff->SetBinContent(iEta_+1,iBin_+1, 0.0);
+            for(int iEta_=0; iEta_<=nEtaBins; iEta_++)
+            {
+                hVzEtaEff[iCent_]->SetBinContent(iEta_+1,iBin_+1, 0.0);
+            }
         }
     }
     
-    for(int iBin_=0; iBin_<=nBins; iBin_++)
+    for(int iCent_=0; iCent_<centBin; iCent_++)
     {
-        char nEventsVzBin[400];
-        sprintf(nEventsVzBin,"TriHadronAnalysisMultMinBias/nEventsVzBin_%d",iBin_);
-        TH1F * hEventsVzBin = (TH1F *)fRead->Get(nEventsVzBin);
-        double nEvents = hEventsVzBin->GetEntries();
-        
-        char ndNdEtaVzBin[400];
-        sprintf(ndNdEtaVzBin,"TriHadronAnalysisMultMinBias/hdNdEta_VzBin_%d",iBin_);
-        hdNdEtaVzBin[iBin_] = (TH1F*)fRead->Get(ndNdEtaVzBin);
-        if(iBin_!=7)		hdNdEtaVzBin[iBin_]->Scale(1.0/nEvents/binWidth);
-        
-        for(int iEta_=0; iEta_<=nEtaBins; iEta_++)
+        for(int iBin_=0; iBin_<=nBins; iBin_++)
         {
+            char nEventsVzBin[400];
+            sprintf(nEventsVzBin,"TriHadronAnalysisMult%d/nEventsVzBin_%d",array,iBin_);
+            cout<<nEventsVzBin<<endl;
+            hEventsVzBin[iCent_] = (TH1F *)fRead->Get(nEventsVzBin);
+            nEvents[iCent_] = hEventsVzBin[iCent_]->GetEntries();
+        
+            char ndNdEtaVzBin[400];
+            sprintf(ndNdEtaVzBin,"TriHadronAnalysisMult%d/hdNdEta_VzBin_%d",array,iBin_);
+            hdNdEtaVzBin[iCent_][iBin_] = (TH1F*)fRead->Get(ndNdEtaVzBin);
+            if(iBin_!=7)		hdNdEtaVzBin[iCent_][iBin_]->Scale(1.0/nEvents[iCent_]/binWidth);
+        
+            for(int iEta_=0; iEta_<=nEtaBins; iEta_++)
+            {
             
-            double numerator = hdNdEtaVzBin[iBin_]->GetBinContent(iEta_+1);
-            double denominator = hSymmetrized->GetBinContent(iEta_+1);
-            if(denominator > 0) ratio = numerator/denominator;
+                numerator[iCent_] = hdNdEtaVzBin[iCent_][iBin_]->GetBinContent(iEta_+1);
+                denominator[iCent_] = hSymmetrized[iCent_].GetBinContent(iEta_+1);
+                if(denominator[iCent_] > 0) ratio[iCent_] = numerator[iCent_]/denominator[iCent_];
             
-         //   cout<<iEta_<<'\t'<<hSymmetrized->GetBinContent(iEta_+1)<<'\t'<<hdNdEtaVzBin[iBin_]->GetBinContent(iEta_+1)<<'\t'<<ratio<<endl;
-            if(hSymmetrized->GetBinContent(iEta_+1) > 0.0 && ratio < 2.0) hVzEtaEff->SetBinContent(iEta_+1,iBin_+1,ratio);
+                //   cout<<iEta_<<'\t'<<hSymmetrized->GetBinContent(iEta_+1)<<'\t'<<hdNdEtaVzBin[iBin_]->GetBinContent(iEta_+1)<<'\t'<<ratio<<endl;
+                if(hSymmetrized[iCent_].GetBinContent(iEta_+1) > 0.0 && ratio[iCent_] < 2.0) hVzEtaEff[iCent_]->SetBinContent(iEta_+1,iBin_+1,ratio[iCent_]);
+            }
         }
-        
-        
     }
     
-    return hVzEtaEff;
+    
+    TFile *fNew = new TFile("CorrectionFactors_PbPb.root", "RECREATE");
+    for(int iCent_=0; iCent_<centBin; iCent_++)
+    {
+        hVzEtaEff[iCent_]->Write();
+    }
+    fNew->Close();
 }
 
-
-void
-CorrectionFactors_MinBias_PbPb::WindowDressing()
-{
-    
-    gStyle->SetOptStat(0);
-    gStyle->SetOptFit(0);
-    gStyle->SetPalette(1);
-    gStyle->SetFrameFillColor(0);
-    gStyle->SetCanvasColor(0);
-    gStyle->SetPadBorderSize(0);
-    gStyle->SetCanvasBorderSize(0);
-    gStyle->SetFrameLineColor(1);
-    gStyle->SetFrameLineWidth(2.0);
-    gStyle->SetCanvasColor(0);
-    gStyle->SetCanvasBorderMode(0);
-    gStyle->SetPadColor(0);
-    gStyle->SetPadBorderMode(0);
-    gStyle->SetPadTickX(1);
-    gStyle->SetPadTickY(1);
-    gStyle->SetLegendBorderSize(0);
-    gStyle->SetErrorX(0.5);
-    
-    gStyle->SetTextFont(42);
-    gStyle->SetLabelFont(42,"x");
-    gStyle->SetLabelFont(42,"y");
-    gStyle->SetLabelFont(42,"z");
-    gStyle->SetTitleFont(42,"x");
-    gStyle->SetTitleFont(42,"y");
-    gStyle->SetTitleFont(42,"z");
-    gStyle->SetEndErrorSize(1);
-    
-    
-}
-
-TCanvas
-*CorrectionFactors_MinBias_PbPb::Canvas()
-{
-    TCanvas *c1 = new TCanvas("c1","C1",700,500);
-    WindowDressing();
-    c1->SetFillColor(0);
-    c1->SetBorderMode(0);
-    c1->SetBorderSize(2);
-    c1->SetTickx(1);
-    c1->SetTicky(1);
-    c1->SetFrameFillStyle(0);
-    c1->SetFrameBorderMode(0);
-    c1->SetTopMargin(0.05);
-    c1->SetBottomMargin(0.1);
-    c1->SetLeftMargin(0.1);
-    c1->SetRightMargin(0.15);
-    c1->SetTheta(50.61189);
-    c1->SetPhi(40.90062);
-    c1->SetLogy();
-    c1->cd();
-    return c1;
-    
-}
-
-TCanvas
-*CorrectionFactors_MinBias_PbPb::Canvas1()
-{
-    TCanvas *c2 = new TCanvas("c2","C2",500,500);
-    WindowDressing();
-    c2->SetFillColor(0);
-    c2->SetBorderMode(0);
-    c2->SetBorderSize(2);
-    c2->SetTickx(1);
-    c2->SetTicky(1);
-    c2->SetFrameFillStyle(0);
-    c2->SetFrameBorderMode(0);
-    c2->SetTopMargin(0.05);
-    c2->SetBottomMargin(0.1);
-    c2->SetLeftMargin(0.1);
-    c2->SetRightMargin(0.15);
-    c2->SetTheta(50.61189);
-    c2->SetPhi(40.90062);
-    c2->cd();
-    return c2;
-
-}
 
