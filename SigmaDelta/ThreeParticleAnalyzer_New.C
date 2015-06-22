dedc8fe9220ac9907b8236f559c3503cb04bbc14
monika0603 <monika.pu@gmail.com>
2015-06-22 13:45:59 +0200

    Removed B0 normalization factor

--- SigmaDelta/ThreeParticleAnalyzer_New.C
+++ SigmaDelta/ThreeParticleAnalyzer_New.C
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <TFile.h>
 #include <TH1.h>
@@ -15,22 +14,22 @@ class ThreeParticleAnalyzer
     ThreeParticleAnalyzer();
     ThreeParticleAnalyzer(std::string fileName);
     TH2D *SameEtaRegion(TFile *fRead, const int cent);
+    TH2D *histoDressing(TH2D *histo);
     TH2D *CrossEtaRegion(TFile *fRead, const int cent);
+    TH2D *CalculateDifference(TH2D *histo1, TH2D *histo2);
     TH2D *CombinatorialBkgTerm(TFile *fRead, const int cent);
     TH2D *CombBkgTerm_crossEtaRegion(TFile *fRead, const int cent);
+   // TH2D *DivideOnly(TH2D *hSignal, TH2D *hBackground);
     TH2D *DivideAndScale(TH2D *hSignal, TH2D *hBackground, TH1F *hEvents);
+    TH2D *SD_sameEtaRegion(TFile *fRead, const int cent);
+    TH2D *SD_crossEtaRegion(TFile *fRead, const int cent);
     void WindowDressing();
-    TCanvas *Canvas();
-    TCanvas *Canvas1();
-    TCanvas *Canvas2();
-    TCanvas *Canvas3();
-    TCanvas *Canvas4();
-    TCanvas *Canvas5();
-    TCanvas *Canvas6();
+    TCanvas *CanvasDressing(int i);
     virtual ~ThreeParticleAnalyzer();
     
   private:
     TFile *fRead;
+    
 };
 
 ThreeParticleAnalyzer::ThreeParticleAnalyzer( std::string fileName )
@@ -40,16 +39,30 @@ ThreeParticleAnalyzer::ThreeParticleAnalyzer( std::string fileName )
 
 ThreeParticleAnalyzer::ThreeParticleAnalyzer()
 {
-    fRead = new TFile("TriHadronCorr_NoEffCorr_FullStat.root");
-    fRead->ls();
-    
-    if(fRead) cout<<"Successfully opned the input file."<<endl;
+
 }
 
 ThreeParticleAnalyzer::~ThreeParticleAnalyzer()
 {
     delete fRead;
 }
+
+/*TH2D *DivideOnly(TH2D *hSignal, TH2D *hBackground)
+{
+    TH1::SetDefaultSumw2();
+    double  _phibinwidth;
+    int _x0, _y0, _B0;
+
+    _phibinwidth = hSignal->GetXaxis()->GetBinWidth(1);
+    
+    _x0 = hBackground->GetXaxis()->FindBin(0.0);
+    _y0 = hBackground->GetYaxis()->FindBin(0.0);
+    _B0 = hBackground->GetBinContent(_x0,_y0);
+    hSignal->Scale(_B0);
+    hSignal->Scale(1.0/_phibinwidth/_phibinwidth);
+    
+    return hSignal;
+}*/
 
 TH2D *DivideAndScale(TH2D *hSignal, TH2D *hBackground, TH1F *hEvents)
 {
@@ -65,7 +78,8 @@ TH2D *DivideAndScale(TH2D *hSignal, TH2D *hBackground, TH1F *hEvents)
     _x0 = hBackground->GetXaxis()->FindBin(0.0);
     _y0 = hBackground->GetYaxis()->FindBin(0.0);
     _B0 = hBackground->GetBinContent(_x0,_y0);
-    hSignal->Scale(_B0/(_nEvents));
+  //  hSignal->Scale(_B0/(_nEvents));
+    hSignal->Scale(_nEvents);
     hSignal->Scale(1.0/_phibinwidth/_phibinwidth);
     
     return hSignal;
@@ -73,17 +87,16 @@ TH2D *DivideAndScale(TH2D *hSignal, TH2D *hBackground, TH1F *hEvents)
 
 TH2D *SameEtaRegion(TFile *fRead, const int cent)
 {
+    // TH1::SetDefaultSumw2();
     TString _Signal_sEtaRegion;
     TString _Background_sEtaRegion;
     TString _Events;
-    
-    double pi_ = 3.1415927;
     
     TH2D *hSignal_sEtaRegion;
     TH2D *hBackground_sEtaRegion;
     TH1F *hEvents;
     
-    TString _centrality[9] = {"0_2", "0_10", "10_20", "20_30", "30_40", "40_50", "50_60", "60_70", "MinBias"};
+    TString _centrality[10] = {"0_2", "0_10", "10_20", "20_30", "30_40", "40_50", "50_60", "60_70", "70_80", "MinBias"};
     TString _signal = "/hSignal0";
     TString _background = "/hBackground0";
     TString _dirName = "TriHadronAnalysisMult";
@@ -92,17 +105,28 @@ TH2D *SameEtaRegion(TFile *fRead, const int cent)
     _Signal_sEtaRegion = _dirName + _centrality[cent] + _signal;
     _Background_sEtaRegion = _dirName + _centrality[cent] + _background;
     _Events = _dirName + _centrality[cent] + _events;
-
-    TH1::SetDefaultSumw2();
     
     hSignal_sEtaRegion = (TH2D*)fRead->Get(_Signal_sEtaRegion);
     hBackground_sEtaRegion = (TH2D*)fRead->Get(_Background_sEtaRegion);
     hEvents = (TH1F*)fRead->Get(_Events);
     
     TH2D *hSig_sameEtaRegion = DivideAndScale(hSignal_sEtaRegion, hBackground_sEtaRegion, hEvents);
-    
     return hSig_sameEtaRegion;
+}
+
+TH2D *histoDressing(TH2D *histo)
+{
+    histo->SetTitle("");
+    histo->GetYaxis()->SetTitle("#Delta#phi_{1}");
+    histo->GetXaxis()->SetTitle("#Delta#phi_{2}");
+    histo->GetXaxis()->SetLabelFont(42);
+    histo->GetXaxis()->SetTitleSize(0.06);
+    histo->GetXaxis()->SetTitleOffset(0.67);
+    histo->GetYaxis()->SetLabelFont(42);
+    histo->GetYaxis()->SetTitleSize(0.06);
+    histo->GetYaxis()->SetTitleOffset(0.67);
     
+    return histo;
 }
 
 TH2D *CrossEtaRegion(TFile *fRead, const int cent)
@@ -110,20 +134,13 @@ TH2D *CrossEtaRegion(TFile *fRead, const int cent)
     TString _Signal_cEtaRegion;
     TString _Background_cEtaRegion;
     TString _Events;
-    
-    double pi_ = 3.1415927;
-    
+
     TH2D *hSignal_cEtaRegion;
     TH2D *hBackground_cEtaRegion;
     TH1F *hEvents;
-    
-    double  _phibinwidth;
-    int _x0, _y0, _B0;
-    double _nEvents;
-    
     TH1::SetDefaultSumw2();
     
-    TString _centrality[9] = {"0_2", "0_10", "10_20", "20_30", "30_40", "40_50", "50_60", "60_70", "MinBias"};
+    TString _centrality[10] = {"0_2", "0_10", "10_20", "20_30", "30_40", "40_50", "50_60", "60_70", "70_80", "MinBias"};
     TString _signal = "/hSignal_af0_as1";
     TString _background = "/hBackground_af0_as1";
     TString _dirName = "TriHadronAnalysisMult";
@@ -141,27 +158,40 @@ TH2D *CrossEtaRegion(TFile *fRead, const int cent)
     return hSig_crossEtaRegion;
 }
 
+TH2D *CalculateDifference(TH2D *histo1, TH2D *histo2)
+{
+    const double pi_ = 3.1415927;
+    TH2D *hDifference = new TH2D("hDifference", "#Delta#phi;#Delta#phi", 96,-pi_/2+pi_/32,3*pi_/2-pi_/32,96,-pi_/2+pi_/32,3*pi_/2-pi_/32);
+    
+    for(int iBin = 1; iBin <=histo1->GetNbinsX(); iBin++)
+    {
+        for(int jBin = 1; jBin <=histo1->GetNbinsY(); jBin++)
+        {
+            int bin = hDifference->GetBin(iBin, jBin);
+            double sameEta = histo1->GetBinContent(iBin, jBin);
+            double crossEta = histo2->GetBinContent(iBin, jBin);
+            double _difference = sameEta - crossEta;
+            hDifference->SetBinContent(iBin, jBin, _difference);
+        }
+    }
+    TH2D *hDifference_D = histoDressing(hDifference);
+    return hDifference_D;
+}
+
 TH2D *CombinatorialBkgTerm(TFile *fRead, const int cent)
 {
     TString _Signal_combBkg0;
     TString _Background_combBkg0;
     TString _Events;
     
-    double pi_ = 3.1415927;
-    
     TH2D *hSignal_combBkg0;
     TH2D *hBackground_combBkg0;
     TH1F *hEvents;
-    
-    double  _phibinwidth;
-    int _x0, _y0, _B0;
-    double _nEvents;
-    
     TH1::SetDefaultSumw2();
     
-    TString _centrality[9] = {"0_2", "0_10", "10_20", "20_30", "30_40", "40_50", "50_60", "60_70", "MinBias"};
+    TString _centrality[10] = {"0_2", "0_10", "10_20", "20_30", "30_40", "40_50", "50_60", "60_70", "70_80", "MinBias"};
     TString _signal = "/hSignal_combBkg0";
-    TString _background = "/hBackground0";
+    TString _background = "/hBkg_combBkg0";
     TString _dirName = "TriHadronAnalysisMult";
     TString _events = "/evtHPNtrk";
     
@@ -183,23 +213,17 @@ TH2D *CombBkgTerm_crossEtaRegion(TFile *fRead, const int cent)
     TString _Background_combBkg0;
     TString _Events;
     
-    double pi_ = 3.1415927;
-    
     TH2D *hSignal_combBkg0_cEtaRegion;
     TH2D *hBackground_combBkg0_cEtaRegion;
     TH1F *hEvents;
-    
-    double  _phibinwidth;
-    int _x0, _y0, _B0;
-    double _nEvents;
-    
     TH1::SetDefaultSumw2();
     
-    TString _centrality[9] = {"0_2", "0_10", "10_20", "20_30", "30_40", "40_50", "50_60", "60_70", "MinBias"};
+    TString _centrality[10] = {"0_2", "0_10", "10_20", "20_30", "30_40", "40_50", "50_60", "60_70", "70_80", "MinBias"};
     TString _signal = "/hSignal_combBkg_af0_as1";
-    TString _background = "/hBackground_af0_as1";
+    TString _background = "/hBkg_combBkg_af0_as1";
     TString _dirName = "TriHadronAnalysisMult";
     TString _events = "/evtHPNtrk";
+  //  TString _events = "/events";
     
     _Signal_combBkg0 = _dirName + _centrality[cent] + _signal;
     _Background_combBkg0 = _dirName + _centrality[cent] + _background;
@@ -213,10 +237,96 @@ TH2D *CombBkgTerm_crossEtaRegion(TFile *fRead, const int cent)
     return hSig_comBkg0_cEtaRegion;
 }
 
+TH2D *SD_sameEtaRegion(TFile *fRead, const int cent)
+{
+    TString _Signal_SD_sEtaRegion;
+    TString _Background_SD_sEtaRegion;
+    TString _Events;
+    
+    TH2D *hSignal_SD_sEtaRegion;
+    TH2D *hBackground_SD_sEtaRegion;
+    TH1F *hEvents;
+    TH1::SetDefaultSumw2();
+    
+    TString _centrality[10] = {"0_2", "0_10", "10_20", "20_30", "30_40", "40_50", "50_60", "60_70", "70_80", "MinBias"};
+    TString _signal = "/hSignal_SD0";
+    TString _background = "/hBackground_SD0";
+    TString _dirName = "TriHadronAnalysisMult";
+    TString _events = "/evtHPNtrk";
+    
+    _Signal_SD_sEtaRegion = _dirName + _centrality[cent] + _signal;
+    _Background_SD_sEtaRegion = _dirName + _centrality[cent] + _background;
+    _Events = _dirName + _centrality[cent] + _events;
+    
+    hSignal_SD_sEtaRegion = (TH2D*)fRead->Get(_Signal_SD_sEtaRegion);
+    hBackground_SD_sEtaRegion = (TH2D*)fRead->Get(_Background_SD_sEtaRegion);
+    hEvents = (TH1F*)fRead->Get(_Events);
+    
+    TH2D *hSig_SD_sEtaRegion = DivideAndScale(hSignal_SD_sEtaRegion, hBackground_SD_sEtaRegion, hEvents);
+    return hSig_SD_sEtaRegion;
+}
+
+TH2D *SD_crossEtaRegion(TFile *fRead, const int cent)
+{
+    TString _Signal_SD_cEtaRegion;
+    TString _Background_SD_cEtaRegion;
+    TString _Events;
+    
+    TH2D *hSignal_SD_cEtaRegion;
+    TH2D *hBackground_SD_cEtaRegion;
+    TH1F *hEvents;
+    TH1::SetDefaultSumw2();
+    
+    TString _centrality[10] = {"0_2", "0_10", "10_20", "20_30", "30_40", "40_50", "50_60", "60_70", "70_80", "MinBias"};
+    TString _signal = "/hSignal_SD_af0_as1";
+    TString _background = "/hBackground_SD0_af0_as1";
+    TString _dirName = "TriHadronAnalysisMult";
+    TString _events = "/evtHPNtrk";
+    
+    _Signal_SD_cEtaRegion = _dirName + _centrality[cent] + _signal;
+    _Background_SD_cEtaRegion = _dirName + _centrality[cent] + _background;
+    _Events = _dirName + _centrality[cent] + _events;
+    
+    hSignal_SD_cEtaRegion = (TH2D*)fRead->Get(_Signal_SD_cEtaRegion);
+    hBackground_SD_cEtaRegion = (TH2D*)fRead->Get(_Background_SD_cEtaRegion);
+    hEvents = (TH1F*)fRead->Get(_Events);
+    
+    TH2D *hSig_SD_cEtaRegion = DivideAndScale(hSignal_SD_cEtaRegion, hBackground_SD_cEtaRegion, hEvents);
+    return hSig_SD_cEtaRegion;
+}
+
+TH2D *SD_combBkg_sameEtaRegion(TFile *fRead, const int cent)
+{
+    TString _Signal_SD_combBkg_sEtaRegion;
+    TString _Bkg_SD_combBkg_sEtaRegion;
+    TString _Events;
+    
+    TH2D *hSignal_SD_combBkg_sEtaRegion;
+    TH2D *hBkg_SD_combBkg_sEtaRegion;
+    TH1F *hEvents;
+    TH1::SetDefaultSumw2();
+    
+    TString _centrality[10] = {"0_2", "0_10", "10_20", "20_30", "30_40", "40_50", "50_60", "60_70", "70_80", "MinBias"};
+    TString _signal = "/hSignal_combBkg_SD0";
+    TString _background = "/hBkg_combBkgSD0";
+    TString _dirName = "TriHadronAnalysisMult";
+    TString _events = "/evtHPNtrk";
+    
+    _Signal_SD_combBkg_sEtaRegion = _dirName + _centrality[cent] + _signal;
+    _Bkg_SD_combBkg_sEtaRegion = _dirName + _centrality[cent] + _background;
+    _Events = _dirName + _centrality[cent] + _events;
+    
+    hSignal_SD_combBkg_sEtaRegion = (TH2D*)fRead->Get(_Signal_SD_combBkg_sEtaRegion);
+    hBkg_SD_combBkg_sEtaRegion = (TH2D*)fRead->Get(_Bkg_SD_combBkg_sEtaRegion);
+    hEvents = (TH1F*)fRead->Get(_Events);
+    
+    TH2D *hSig_SD_sEtaRegion = DivideAndScale(hSignal_SD_combBkg_sEtaRegion, hBkg_SD_combBkg_sEtaRegion, hEvents);
+    return hSig_SD_sEtaRegion;
+}
+
 void
 ThreeParticleAnalyzer::WindowDressing()
 {
-    
     gStyle->SetOptStat(0);
     gStyle->SetOptFit(0);
     gStyle->SetPalette(1);
@@ -245,165 +355,28 @@ ThreeParticleAnalyzer::WindowDressing()
 }
 
 TCanvas
-*ThreeParticleAnalyzer::Canvas()
+*ThreeParticleAnalyzer::CanvasDressing(int i)
 {
-    TCanvas *c1 = new TCanvas("c1","C1",700,500);
-    WindowDressing();
-    c1->SetFillColor(0);
-    c1->SetBorderMode(0);
-    c1->SetBorderSize(2);
-    c1->SetTickx(1);
-    c1->SetTicky(1);
-    c1->SetFrameFillStyle(0);
-    c1->SetFrameBorderMode(0);
-    c1->SetTopMargin(0.05);
-    c1->SetBottomMargin(0.1);
-    c1->SetLeftMargin(0.1);
-    c1->SetRightMargin(0.15);
-    c1->SetTheta(50.61189);
-    c1->SetPhi(40.90062);
-    c1->cd();
-    return c1;
+    TString name = "c";
+    char numstr[21];
+    sprintf(numstr, "%d", i);
+    TString result = name + numstr;
     
-}
-
-TCanvas
-*ThreeParticleAnalyzer::Canvas1()
-{
-    TCanvas *c2 = new TCanvas("c2","C2",700,500);
+    TCanvas *c = new TCanvas(result,result,700,500);
     WindowDressing();
-    c2->SetFillColor(0);
-    c2->SetBorderMode(0);
-    c2->SetBorderSize(2);
-    c2->SetTickx(1);
-    c2->SetTicky(1);
-    c2->SetFrameFillStyle(0);
-    c2->SetFrameBorderMode(0);
-    c2->SetTopMargin(0.05);
-    c2->SetBottomMargin(0.1);
-    c2->SetLeftMargin(0.1);
-    c2->SetRightMargin(0.15);
-    c2->SetTheta(50.61189);
-    c2->SetPhi(40.90062);
-    c2->cd();
-    return c2;
-
+    c->SetFillColor(0);
+    c->SetBorderMode(0);
+    c->SetBorderSize(2);
+    c->SetTickx(1);
+    c->SetTicky(1);
+    c->SetFrameFillStyle(0);
+    c->SetFrameBorderMode(0);
+    c->SetTopMargin(0.05);
+    c->SetBottomMargin(0.1);
+    c->SetLeftMargin(0.1);
+    c->SetRightMargin(0.15);
+    c->SetTheta(50.61189);
+    c->SetPhi(40.90062);
+    c->cd();
+    return c;
 }
-
-TCanvas
-*ThreeParticleAnalyzer::Canvas2()
-{
-    TCanvas *c3 = new TCanvas("c3","C3",700,500);
-    WindowDressing();
-    c3->SetFillColor(0);
-    c3->SetBorderMode(0);
-    c3->SetBorderSize(2);
-    c3->SetTickx(1);
-    c3->SetTicky(1);
-    c3->SetFrameFillStyle(0);
-    c3->SetFrameBorderMode(0);
-    c3->SetTopMargin(0.05);
-    c3->SetBottomMargin(0.1);
-    c3->SetLeftMargin(0.1);
-    c3->SetRightMargin(0.15);
-    c3->SetTheta(50.61189);
-    c3->SetPhi(40.90062);
-    c3->cd();
-    return c3;
-    
-}
-
-TCanvas
-*ThreeParticleAnalyzer::Canvas3()
-{
-    TCanvas *c4 = new TCanvas("c4","C4",700,500);
-    WindowDressing();
-    c4->SetFillColor(0);
-    c4->SetBorderMode(0);
-    c4->SetBorderSize(2);
-    c4->SetTickx(1);
-    c4->SetTicky(1);
-    c4->SetFrameFillStyle(0);
-    c4->SetFrameBorderMode(0);
-    c4->SetTopMargin(0.05);
-    c4->SetBottomMargin(0.1);
-    c4->SetLeftMargin(0.1);
-    c4->SetRightMargin(0.15);
-    c4->SetTheta(50.61189);
-    c4->SetPhi(40.90062);
-    c4->cd();
-    return c4;
-    
-}
-
-TCanvas
-*ThreeParticleAnalyzer::Canvas4()
-{
-    TCanvas *c5 = new TCanvas("c5","C5",700,500);
-    WindowDressing();
-    c5->SetFillColor(0);
-    c5->SetBorderMode(0);
-    c5->SetBorderSize(2);
-    c5->SetTickx(1);
-    c5->SetTicky(1);
-    c5->SetFrameFillStyle(0);
-    c5->SetFrameBorderMode(0);
-    c5->SetTopMargin(0.05);
-    c5->SetBottomMargin(0.1);
-    c5->SetLeftMargin(0.1);
-    c5->SetRightMargin(0.15);
-    c5->SetTheta(50.61189);
-    c5->SetPhi(40.90062);
-    c5->cd();
-    return c5;
-    
-}
-
-TCanvas
-*ThreeParticleAnalyzer::Canvas5()
-{
-    TCanvas *c6 = new TCanvas("c6","C6",700,500);
-    WindowDressing();
-    c6->SetFillColor(0);
-    c6->SetBorderMode(0);
-    c6->SetBorderSize(2);
-    c6->SetTickx(1);
-    c6->SetTicky(1);
-    c6->SetFrameFillStyle(0);
-    c6->SetFrameBorderMode(0);
-    c6->SetTopMargin(0.05);
-    c6->SetBottomMargin(0.1);
-    c6->SetLeftMargin(0.1);
-    c6->SetRightMargin(0.15);
-    c6->SetTheta(50.61189);
-    c6->SetPhi(40.90062);
-    c6->cd();
-    return c6;
-    
-}
-
-TCanvas
-*ThreeParticleAnalyzer::Canvas6()
-{
-    TCanvas *c7 = new TCanvas("c7","C7",700,500);
-    WindowDressing();
-    c7->SetFillColor(0);
-    c7->SetBorderMode(0);
-    c7->SetBorderSize(2);
-    c7->SetTickx(1);
-    c7->SetTicky(1);
-    c7->SetFrameFillStyle(0);
-    c7->SetFrameBorderMode(0);
-    c7->SetTopMargin(0.05);
-    c7->SetBottomMargin(0.1);
-    c7->SetLeftMargin(0.1);
-    c7->SetRightMargin(0.15);
-    c7->SetTheta(50.61189);
-    c7->SetPhi(40.90062);
-    c7->cd();
-    return c7;
-   
-    
-}
-
-
--- SigmaDelta/ThreeParticleMacro_unlocked.C
+++ SigmaDelta/ThreeParticleMacro_unlocked.C
@@ -5,151 +5,64 @@ void ThreeParticleMacro_unlocked()
     
     ThreeParticleAnalyzer *cf = new ThreeParticleAnalyzer();
     
-    TCanvas *c1 = cf->Canvas();
-    TFile *fRead = new TFile("TriHadronCorr_NoEffCorr_FullStat.root");
-    TH2D *hSig_sameEtaRegion = SameEtaRegion(fRead, 0);
-    hSig_sameEtaRegion->SetTitle("");
-    hSig_sameEtaRegion->GetYaxis()->SetTitle("#Delta#phi");
-    hSig_sameEtaRegion->GetXaxis()->SetTitle("#Delta#phi");
-    hSig_sameEtaRegion->GetXaxis()->SetLabelFont(42);
-    hSig_sameEtaRegion->GetXaxis()->SetTitleSize(0.06);
-    hSig_sameEtaRegion->GetXaxis()->SetTitleOffset(0.67);
-    hSig_sameEtaRegion->GetYaxis()->SetLabelFont(42);
-    hSig_sameEtaRegion->GetYaxis()->SetTitleSize(0.06);
-    hSig_sameEtaRegion->GetYaxis()->SetTitleOffset(0.67);
-    hSig_sameEtaRegion->Draw("zcol");
+    int Centrality = 0;
+    
+    TCanvas *c1 = cf->CanvasDressing(1);
+    TFile *fRead = new TFile("TriHadronCorrSigmaDeltaFullStat.root");
+    TH2D *hSig_sameEtaRegion = SameEtaRegion(fRead, Centrality);
+    TH2D *hDSig_sameEtaRegion = histoDressing(hSig_sameEtaRegion);
+    hDSig_sameEtaRegion->Draw("zcol");
     c1->SaveAs("HistSameEtaRegion.pdf");
     
-    TCanvas *c2 = cf->Canvas1();
-    TH2D *hSig_crossEtaRegion = CrossEtaRegion(fRead, 0);
-    hSig_crossEtaRegion->GetYaxis()->SetTitle("#Delta#phi");
-    hSig_crossEtaRegion->GetXaxis()->SetTitle("#Delta#phi");
-    hSig_crossEtaRegion->GetXaxis()->SetLabelFont(42);
-    hSig_crossEtaRegion->GetXaxis()->SetTitleSize(0.06);
-    hSig_crossEtaRegion->GetXaxis()->SetTitleOffset(0.67);
-    hSig_crossEtaRegion->GetYaxis()->SetLabelFont(42);
-    hSig_crossEtaRegion->GetYaxis()->SetTitleSize(0.06);
-    hSig_crossEtaRegion->GetYaxis()->SetTitleOffset(0.67);
-    hSig_crossEtaRegion->SetTitle("");
-    hSig_crossEtaRegion->Draw("zcol");
+    TCanvas *c2 = cf->CanvasDressing(2);
+    TH2D *hSig_crossEtaRegion = CrossEtaRegion(fRead, Centrality);
+    TH2D *hDSig_crossEtaRegion = histoDressing(hSig_crossEtaRegion);
+    hDSig_crossEtaRegion->Draw("zcol");
     c2->SaveAs("HistCrossEtaRegion.pdf");
     
-    TCanvas *c3 = cf->Canvas2();
-    const double pi_ = 3.1415927;
-    TH2D *hDifference = new TH2D("hDifference", "#Delta#phi;#Delta#phi", 96,-pi_/2+pi_/32,3*pi_/2-pi_/32,96,-pi_/2+pi_/32,3*pi_/2-pi_/32);
-    
-    for(int iBin = 1; iBin <=hSig_sameEtaRegion->GetNbinsX(); iBin++)
-    {
-        for(int jBin = 1; jBin <=hSig_crossEtaRegion->GetNbinsY(); jBin++)
-        {
-            int bin = hDifference->GetBin(iBin, jBin);
-            double sameEta = hSig_sameEtaRegion->GetBinContent(iBin, jBin);
-            double crossEta = hSig_crossEtaRegion->GetBinContent(iBin, jBin);
-            double _difference = sameEta - crossEta;
-            hDifference->SetBinContent(iBin, jBin, _difference);
-        }
-    }
-    hDifference->GetYaxis()->SetTitle("#Delta#phi");
-    hDifference->GetXaxis()->SetTitle("#Delta#phi");
-    hDifference->GetXaxis()->SetLabelFont(42);
-    hDifference->GetXaxis()->SetTitleSize(0.06);
-    hDifference->GetXaxis()->SetTitleOffset(0.67);
-    hDifference->GetYaxis()->SetLabelFont(42);
-    hDifference->GetYaxis()->SetTitleSize(0.06);
-    hDifference->GetYaxis()->SetTitleOffset(0.67);
-    hDifference->SetTitle("");
-    hDifference->Draw("zcol");
+    TCanvas *c3 = cf->CanvasDressing(3);
+    TH2D *hDiffRealCorr = CalculateDifference(hSig_sameEtaRegion, hSig_crossEtaRegion);
+    hDiffRealCorr->Draw("zcol");
     c3->SaveAs("SameMinusCrossEtaRegion.pdf");
     
-    TCanvas *c4 = cf->Canvas3();
-    TH2D *hSig_combBkgTerm = CombinatorialBkgTerm(fRead, 0);
-    hSig_combBkgTerm->GetYaxis()->SetTitle("#Delta#phi");
-    hSig_combBkgTerm->GetXaxis()->SetTitle("#Delta#phi");
-    hSig_combBkgTerm->GetXaxis()->SetLabelFont(42);
-    hSig_combBkgTerm->GetXaxis()->SetTitleSize(0.06);
-    hSig_combBkgTerm->GetXaxis()->SetTitleOffset(0.67);
-    hSig_combBkgTerm->GetYaxis()->SetLabelFont(42);
-    hSig_combBkgTerm->GetYaxis()->SetTitleSize(0.06);
-    hSig_combBkgTerm->GetYaxis()->SetTitleOffset(0.67);
-    hSig_combBkgTerm->Draw("zcol");
-    hSig_combBkgTerm->SetTitle("");
+    TCanvas *c4 = cf->CanvasDressing(4);
+    TH2D *hSig_combBkgTerm = CombinatorialBkgTerm(fRead, Centrality);
+    TH2D *hDSig_combBkgTerm = histoDressing(hSig_combBkgTerm);
+    hDSig_combBkgTerm->Draw("zcol");
     c4->SaveAs("SameEtaRegion_combBkgTerm.pdf");
     
-    
-    TCanvas *c5 = cf->Canvas4();
-    TH2D *hSig_combBkgTerm_cEtaRegion = CombBkgTerm_crossEtaRegion(fRead, 0);
-    hSig_combBkgTerm_cEtaRegion->GetYaxis()->SetTitle("#Delta#phi");
-    hSig_combBkgTerm_cEtaRegion->GetXaxis()->SetTitle("#Delta#phi");
-    hSig_combBkgTerm_cEtaRegion->GetXaxis()->SetLabelFont(42);
-    hSig_combBkgTerm_cEtaRegion->GetXaxis()->SetTitleSize(0.06);
-    hSig_combBkgTerm_cEtaRegion->GetXaxis()->SetTitleOffset(0.67);
-    hSig_combBkgTerm_cEtaRegion->GetYaxis()->SetLabelFont(42);
-    hSig_combBkgTerm_cEtaRegion->GetYaxis()->SetTitleSize(0.06);
-    hSig_combBkgTerm_cEtaRegion->GetYaxis()->SetTitleOffset(0.67);
-    hSig_combBkgTerm_cEtaRegion->Draw("zcol");
-    hSig_combBkgTerm_cEtaRegion->SetTitle("");
+    TCanvas *c5 = cf->CanvasDressing(5);
+    TH2D *hSig_combBkgTerm_cEtaRegion = CombBkgTerm_crossEtaRegion(fRead, Centrality);
+    TH2D *hDSig_combBkgTerm_cEtaRegion = histoDressing(hSig_combBkgTerm_cEtaRegion);
+    hDSig_combBkgTerm_cEtaRegion->Draw("zcol");
     c5->SaveAs("CrossEtaRegion_combBkgTerm.pdf");
-    
-    TCanvas *c6 = cf->Canvas5();
-    TH2D *hDiffBkgTerm = new TH2D("hDiffBkgTerm", "#Delta#phi;#Delta#phi", 96,-pi_/2+pi_/32,3*pi_/2-pi_/32,96,-pi_/2+pi_/32,3*pi_/2-pi_/32);
-     
-    for(int iBin = 1; iBin <=hSig_combBkgTerm->GetNbinsX(); iBin++)
-    {
-        for(int jBin = 1; jBin <=hSig_combBkgTerm_cEtaRegion->GetNbinsY(); jBin++)
-            {
-                int bin = hDiffBkgTerm->GetBin(iBin, jBin);
-                double sameEtaBkgTerm = hSig_combBkgTerm->GetBinContent(iBin, jBin);
-                double crossEtaBkgTerm = hSig_combBkgTerm_cEtaRegion->GetBinContent(iBin, jBin);
-                double _difference = sameEtaBkgTerm - crossEtaBkgTerm;
-                hDiffBkgTerm->SetBinContent(iBin, jBin, _difference);
-            }
-    }
-     
-    hDiffBkgTerm->GetYaxis()->SetTitle("#Delta#phi");
-    hDiffBkgTerm->GetXaxis()->SetTitle("#Delta#phi");
-    hDiffBkgTerm->GetXaxis()->SetLabelFont(42);
-    hDiffBkgTerm->GetXaxis()->SetTitleSize(0.06);
-    hDiffBkgTerm->GetXaxis()->SetTitleOffset(0.67);
-    hDiffBkgTerm->GetYaxis()->SetLabelFont(42);
-    hDiffBkgTerm->GetYaxis()->SetTitleSize(0.06);
-    hDiffBkgTerm->GetYaxis()->SetTitleOffset(0.67);
+
+    TCanvas *c6 = cf->CanvasDressing(6);
+    TH2D *hDiffBkgTerm = CalculateDifference(hSig_combBkgTerm, hSig_combBkgTerm_cEtaRegion);
     hDiffBkgTerm->Draw("zcol");
-    hDiffBkgTerm->SetTitle("");
     c6->SaveAs("SameMinusCrossEtaRegion_combBkgTerm.pdf");
 
-
     ////////////////////////////////////////////////////////////////////////
-    TCanvas *c7 = cf->Canvas6();
-    TH2D *hFinalSignal = new TH2D("hFinalSignal", "#Delta#phi;#Delta#phi", 96,-pi_/2+pi_/32,3*pi_/2-pi_/32,96,-pi_/2+pi_/32,3*pi_/2-pi_/32);
-    
-    for(int iBin = 1; iBin <=hDifference->GetNbinsX(); iBin++)
-    {
-        for(int jBin = 1; jBin <=hDiffBkgTerm->GetNbinsY(); jBin++)
-        {
-            int bin = hFinalSignal->GetBin(iBin, jBin);
-            double RealCorrEtaDiff = hDifference->GetBinContent(iBin, jBin);
-            double combBkgTermDiff = hDiffBkgTerm->GetBinContent(iBin, jBin);
-            double _difference = RealCorrEtaDiff - combBkgTermDiff;
-            hFinalSignal->SetBinContent(iBin, jBin, _difference);
-        }
-    }
-    
-    hFinalSignal->GetYaxis()->SetTitle("#Delta#phi");
-    hFinalSignal->GetXaxis()->SetTitle("#Delta#phi");
-    hFinalSignal->GetXaxis()->SetLabelFont(42);
-    hFinalSignal->GetXaxis()->SetTitleSize(0.06);
-    hFinalSignal->GetXaxis()->SetTitleOffset(0.67);
-    hFinalSignal->GetYaxis()->SetLabelFont(42);
-    hFinalSignal->GetYaxis()->SetTitleSize(0.06);
-    hFinalSignal->GetYaxis()->SetTitleOffset(0.67);
+    TCanvas *c7 = cf->CanvasDressing(7);
+    TH2D *hFinalSignal = CalculateDifference(hDiffRealCorr, hDiffBkgTerm);
     hFinalSignal->Draw("zcol");
-    hFinalSignal->SetTitle("");
     c7->SaveAs("SignalMinusCombBkgTerm.pdf");
     
-   /* TFile *fNew = new TFile("TriHadronAnalyzerOutput_PbPb.root", "RECREATE");
-    hFactors->Write();
-    fNew->Close();*/
+    TCanvas *c8 = cf->CanvasDressing(8);
+    TH2D *hSig_SD_sEtaRegion = SD_sameEtaRegion(fRead, Centrality);
+    TH2D *hDSig_SD_sEtaRegion = histoDressing(hSig_SD_sEtaRegion);
+    hDSig_SD_sEtaRegion->Draw("zcol");
+    c8->SaveAs("SDSameEtaRegion.pdf");
     
+    TCanvas *c9 = cf->CanvasDressing(9);
+    TH2D *hSig_SD_cEtaRegion = SD_crossEtaRegion(fRead, Centrality);
+    TH2D *hDSig_SD_cEtaRegion = histoDressing(hSig_SD_cEtaRegion);
+    hDSig_SD_cEtaRegion->Draw("zcol");
+    c9->SaveAs("SDCrossEtaRegion.pdf");
+    
+    TCanvas *c10 = cf->CanvasDressing(10);
+    TH2D *hDiffSigmaDelta = CalculateDifference(hSig_SD_sEtaRegion, hSig_SD_cEtaRegion);
+    hDiffSigmaDelta->Draw("zcol");
 }
 
 
